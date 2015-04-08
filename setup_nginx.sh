f244e0db4a9712cb4ebcc79a0444519e64b2e1c5
Kirill Shmakov <shmakir@yandex.ru>
2015-04-08 13:42:56 +0300

    Install scripts updated.

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -20,23 +20,24 @@ mv $PCRE pcre_src
 # Make
 ###################################
 
-mkdir -p ./nginx/logs
-mkdir -p ./nginx/tmp/fcgi
-
 BASE_DIR=`pwd`
-INSTALLATION_DIR=$BASE_DIR/nginx
+INSTALL_DIR=$BASE_DIR/nginx
+
+mkdir -p $INSTALL_DIR/logs
+mkdir -p $INSTALL_DIR/tmp/fcgi
+touch $INSTALL_DIR/tmp/citations_serv.sock
 
 cd nginx_src && ./configure \
-    --prefix=$INSTALLATION_DIR \
-    --sbin-path=$INSTALLATION_DIR/sbin/nginx \
-    --conf-path=$INSTALLATION_DIR/conf/nginx.conf \
-    --error-log-path=$INSTALLATION_DIR/logs/error.log \
-    --http-log-path=$INSTALLATION_DIR/logs/access.log \
-    --pid-path=$INSTALLATION_DIR/tmp/nginx.pid \
-    --lock-path=$INSTALLATION_DIR/tmp/nginx.lock \
-    --http-fastcgi-temp-path=$INSTALLATION_DIR/tmp/fcgi \
-    --http-client-body-temp-path=$INSTALLATION_DIR/tmp/client_body \
-    --http-proxy-temp-path=$INSTALLATION_DIR/tmp/proxy \
+    --prefix=$INSTALL_DIR \
+    --sbin-path=$INSTALL_DIR/sbin/nginx \
+    --conf-path=$INSTALL_DIR/conf/nginx.conf \
+    --error-log-path=$INSTALL_DIR/logs/error.log \
+    --http-log-path=$INSTALL_DIR/logs/access.log \
+    --pid-path=$INSTALL_DIR/tmp/nginx.pid \
+    --lock-path=$INSTALL_DIR/tmp/nginx.lock \
+    --http-fastcgi-temp-path=$INSTALL_DIR/tmp/fcgi \
+    --http-client-body-temp-path=$INSTALL_DIR/tmp/client_body \
+    --http-proxy-temp-path=$INSTALL_DIR/tmp/proxy \
     --with-pcre=$BASE_DIR/pcre_src \
     --without-mail_pop3_module \
     --without-mail_imap_module \
@@ -53,4 +54,5 @@ cd nginx_src && ./configure \
     --without-http_browser_module
 
 make && make install && cd ..
-cp -f nginx.conf ./nginx/conf/nginx.conf
+sed "s#INSTALL_DIR#$INSTALL_DIR#g" nginx.conf.pre > ./nginx/conf/nginx.conf
+
