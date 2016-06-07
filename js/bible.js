a512cb3596248a3dd4a1cc395cf2b6a793124556
menato <shmakir@yandex.ru>
2016-06-07 16:31:15 +0300

    Books and settings menu added.

--- js/bible.js
+++ js/bible.js
@@ -2,11 +2,21 @@ import { createStore } from 'redux'
 import fetch from 'isomorphic-fetch'
 
 const persistedState = {
-    mainLang: localStorage.getItem("mainLang") || _global.mainLang
+    mainLang: localStorage.getItem('mainLang') || 'ru',
+    menu: 'NONE'
 };
 
-function bibleApp(state = {}, action) {
+function bibleApp(state, action) {
     switch (action.type) {
+        case 'MENU_SETTINGS':
+            state['menu'] = 'SETTINGS';
+            return state;
+        case 'MENU_BOOKS':
+            state['menu'] = 'BOOKS';
+            return state;
+        case 'MENU_NONE':
+            state['menu'] = 'NONE';
+            return state;
         case 'RESET_VERSE':
             state[action.verse] = {
                 info: undefined,
@@ -31,31 +41,75 @@ let store = createStore(bibleApp, persistedState);
 
 store.subscribe(() => {
     const state = store.getState();
-    console.log("mainLang = " + state.mainLang);
     for (var verseId in state) {
-        var wrap   = document.getElementById(state[verseId].wrap);
-        var top    = document.getElementById(state[verseId].top);
-        var bottom = document.getElementById(state[verseId].bottom);
+        if (!isNaN(verseId)) {
+            var wrap   = document.getElementById(state[verseId].wrap);
+            var top    = document.getElementById(state[verseId].top);
+            var bottom = document.getElementById(state[verseId].bottom);
 
-        top.className = (state[verseId].open
-            ? "specified-verse"
-            : "specifiable-verse");
+            top.className = (state[verseId].open
+                ? "specified-verse"
+                : "specifiable-verse");
 
-        if (state[verseId].open && !wrap.contains(bottom)) {
-            bottom = document.createElement("div");
-            bottom.id = state[verseId].bottom;
-            bottom.classList.add("specification")
-            bottom.appendChild(document.createTextNode(JSON.stringify(state[verseId].info)));
-            wrap.appendChild(bottom);
-        }
+            if (state[verseId].open && !wrap.contains(bottom)) {
+                bottom = document.createElement("div");
+                bottom.id = state[verseId].bottom;
+                bottom.classList.add("specification");
+                bottom.appendChild(document.createTextNode(JSON.stringify(state[verseId].info)));
+                wrap.appendChild(bottom);
+            }
 
-        if (!state[verseId].open && wrap.contains(bottom)) {
-            wrap.removeChild(bottom);
+            if (!state[verseId].open && wrap.contains(bottom)) {
+                wrap.removeChild(bottom);
+            }
         }
     }
 });
 
-_global.toggle = function (verse, wrap, top) {
+let menuValue='NONE';
+
+store.subscribe(() => {
+    if (menuValue === store.getState()['menu']) {
+        return;
+    }
+
+    menuValue = store.getState()['menu'];
+
+    var container = document.getElementById("container");
+    var menu = document.getElementById("menu");
+
+    if (container.contains(menu)) {
+        container.removeChild(menu);
+    }
+
+    if (menuValue === 'NONE') {
+        return;
+    }
+
+    var padding = document.createElement("div");
+    padding.className = "col-md-2 col-sm-1";
+    var center = document.createElement("div");
+    center.className = "col-md-8 col-sm-10";
+
+    if (menuValue === 'BOOKS') {
+        center.appendChild(document.createTextNode("Books."));
+    } else if (menuValue === 'SETTINGS') {
+        center.appendChild(document.createTextNode("Settings."));
+    }
+
+    center.appendChild(document.createElement("hr"));
+
+    menu = document.createElement("div");
+    menu.className = "row";
+    menu.id = "menu";
+    menu.appendChild(padding);
+    menu.appendChild(center);
+    menu.appendChild(padding.cloneNode(true));
+
+    container.insertBefore(menu, container.childNodes[3]);
+});
+
+_global.toggleVerse = function (verse, wrap, top) {
     if (!store.getState()[verse]) {
         store.dispatch({type: 'RESET_VERSE', verse: verse, wrap: wrap, top: top});
         fetch(`/specification/${_global.book}/${_global.chapter}/${verse}`)
@@ -70,10 +124,19 @@ _global.toggle = function (verse, wrap, top) {
 };
 
 _global.toggleSettings = function() {
-    var settingsDiv = document.createElement("div");
-    settingsDiv.appendChild(document.createTextNode("Ho ho ho"));
-    var container = document.getElementById("container");
-    container.insertBefore(settingsDiv, container.childNodes[0]);
+    if (store.getState()['menu'] === 'SETTINGS') {
+        store.dispatch({type: 'MENU_NONE'});
+    } else {
+        store.dispatch({type: 'MENU_SETTINGS'});
+    }
+};
+
+_global.toggleBooks = function() {
+    if (store.getState()['menu'] === 'BOOKS') {
+        store.dispatch({type: 'MENU_NONE'});
+    } else {
+        store.dispatch({type: 'MENU_BOOKS'});
+    }
 };
 
 
