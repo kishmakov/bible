4de1ceec88bc3d4a2bebc65796abb086ab6446c2
menato <shmakir@yandex.ru>
2016-09-26 19:21:02 +0300

    DB scheme for parallel places provided.

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,117 +1,20 @@
 # --- !Ups
 
-insert into `headers` values
-    ("ru",    "Bib",                            "Библия",       null, null,  null),
-    ("ru",  "Incan",                    "Неканоническая",       null, null,  null),
-    ("ru",   "Lang",                              "Язык",       null, null,  null),
-    ("ru", "SLangs",                  "Уточняющие языки",       null, null,  null),
+create table `parallel` (
+  `lang` VARCHAR(5) not null,
+  `src_book_id` VARCHAR(10) not null,
+  `src_chapter` INTEGER not null,
+  `src_verse` INTEGER not null,
+  `dst_book_id` VARCHAR(10) not null,
+  `dst_chapter` INTEGER not null,
+  `dst_verse` INTEGER not null);
 
-    ("ru",     "VT",                      "Ветхий Завет",       null, null,  null),
-
-    ("ru",  "StatV",                 "Пятикнижие Моисея",       null, "Книги законоположительные",  null),
-
-    ("ru",    "Gen",                             "Бытие",     "Быт.", null, false),
-    ("ru",     "Ex",                             "Исход",     "Исх.", null, false),
-    ("ru",    "Lev",                             "Левит",     "Лев.", null, false),
-    ("ru",    "Num",                             "Числа",     "Чис.", null, false),
-    ("ru",   "Deut",                      "Второзаконие",    "Втор.", null, false),
-
-    ("ru",  "HistV",                "Книги исторические",       null, null,  null),
-
-    ("ru",    "Jos",                     "Иисуса Навина",     "Нав.", null, false),
-    ("ru",   "Jdcs",                             "Судей",     "Суд.", null, false),
-    ("ru",   "Ruth",                              "Руфи",     "Руф.", null, false),
-    ("ru",   "1Reg",                        "1-я Царств",    "1Цар.", null, false),
-    ("ru",   "2Reg",                        "2-я Царств",    "2Цар.", null, false),
-    ("ru",   "3Reg",                        "3-я Царств",    "3Цар.", null, false),
-    ("ru",   "4Reg",                        "4-я Царств",    "4Цар.", null, false),
-    ("ru",   "1Par",                 "1-я Паралипоменон",    "1Пар.", null, false),
-    ("ru",   "2Par",                 "2-я Паралипоменон",    "2Пар.", null, false),
-    ("ru",   "1Esd",                         "1-я Ездры",    "1Езд.", null, false),
-    ("ru",   "2Esd",                         "2-я Ездры",    "2Езд.", "Неемии", false),
-    ("ru",   "3Esd",                         "3-я Ездры",    "3Езд.", null, false),
-    ("ru",   "4Esd",                         "4-я Ездры",    "4Езд.", null, false),
-    ("ru",    "Tob",                            "Товита",     "Тов.", null, false),
-    ("ru",   "Jdth",                            "Иудифи",   "Иудифь", null, false),
-    ("ru",   "Esth",                            "Эсфири",     "Эсф.", null, false),
-    ("ru",  "1Mach",                     "1-я Маккавеев",    "1Мак.", null, false),
-    ("ru",  "2Mach",                     "2-я Маккавеев",    "2Мак.", null, false),
-    ("ru",  "3Mach",                     "3-я Маккавеев",    "3Мак.", null, false),
-    ("ru",  "4Mach",                     "4-я Маккавеев",    "4Мак.", null, false),
-
-    ("ru",   "DocV",                  "Книги учительные",       null, null,  null),
-
-    ("ru",    "Job",                              "Иова",      "Иов", null, false),
-    ("ru",     "Ps",                          "Псалтирь",      "Пс.", null, false),
-    ("ru",   "Prov",               "Притчей Соломоновых",    "Прит.", null, false),
-    ("ru",    "Ecc",                       "Екклезиаста",    "Еккл.", null, false),
-    ("ru",     "CC",             "Песни Песней Соломона",    "Песн.", null, false),
-    ("ru",    "Sap",              "Премудрости Соломона",    "Прем.", null, false),
-    ("ru",    "Sir", "Премудрости Иисуса, сына Сирахова",     "Сир.", null, false),
-
-    ("ru",  "ProvV",                 "Книги пророческие",       null, null,  null),
-
-    ("ru",    "Isa",                             "Исаии",      "Ис.", null, false),
-    ("ru",    "Jer",                           "Иеремии",     "Иер.", null, false),
-    ("ru",   "Ezch",                         "Иезекииля",     "Иез.", null, false),
-    ("ru",    "Dan",                           "Даниила",     "Дан.", null, false),
-    ("ru",    "Lam",                      "Плач Иеремии",    "Плач.", null, false),
-    ("ru",   "EJer",                  "Послание Иеремии", "ПослИер.", null, false),
-    ("ru",    "Bar",                            "Варуха",     "Вар.", null, false),
-    ("ru",   "Osee",                              "Осии",      "Ос.", null, false),
-    ("ru",   "Joel",                             "Иoиля",    "Иоил.", null, false),
-    ("ru",     "Am",                             "Амоса",      "Ам.", null, false),
-    ("ru",    "Abd",                             "Авдия",     "Авд.", null, false),
-    ("ru",    "Jon",                              "Ионы",     "Ион.", null, false),
-    ("ru",    "Mic",                             "Михея",     "Мих.", null, false),
-    ("ru",    "Nah",                             "Наума",    "Наум.", null, false),
-    ("ru",    "Hab",                          "Аввакума",     "Авв.", null, false),
-    ("ru",   "Soph",                           "Софонии",     "Соф.", null, false),
-    ("ru",    "Agg",                             "Аггея",     "Агг.", null, false),
-    ("ru",   "Zach",                           "Захарии",     "Зах.", null, false),
-    ("ru",    "Mal",                           "Малахии",     "Мал.", null, false),
-
-    ("ru",     "NT",                       "Новый Завет",       null, null,  null),
-
-    ("ru",  "StatN",                         "Евангелие",       null, "Книги законоположительные",  null),
-
-    ("ru",     "Mt",               "Евангелие по Матфею",      "Мф.", null, false),
-    ("ru",     "Mc",                "Евангелие по Марку",      "Мк.", null, false),
-    ("ru",     "Lc",                 "Евангелие по Луке",      "Лк.", null, false),
-    ("ru",     "In",               "Евангелие по Иоанну",      "Ин.", null, false),
-
-    ("ru",  "HistN",                "Книги исторические",       null, null,  null),
-
-    ("ru",    "Act",           "Деяния святых Апостолов",    "Деян.", null, false),
-
-    ("ru",   "DocN",                  "Книги учительные",       null, null,  null),
-
-    ("ru",    "Jac",                            "Иакова",     "Иак.", null, false),
-    ("ru",   "1Pet",                         "1-е Петра",    "1Пет.", null, false),
-    ("ru",   "2Pet",                         "2-е Петра",    "2Пет.", null, false),
-    ("ru",    "1In",              "1-е Иоанна Богослова",     "1Ин.", null, false),
-    ("ru",    "2In",              "2-е Иоанна Богослова",     "2Ин.", null, false),
-    ("ru",    "3In",              "3-е Иоанна Богослова",     "3Ин.", null, false),
-    ("ru",     "Jd",                              "Иуды",     "Иуд.", null, false),
-    ("ru",    "Rom",                        "К римлянам",     "Рим.", null, false),
-    ("ru",   "1Cor",                  "К коринфянам 1-е",    "1Кор.", null, false),
-    ("ru",   "2Cor",                  "К коринфянам 2-е",    "2Кор.", null, false),
-    ("ru",    "Gal",                         "К галатам",     "Гал.", null, false),
-    ("ru",    "Eph",                        "К ефесянам",      "Еф.", null, false),
-    ("ru",    "Php",                     "К филиппийцам",     "Фил.", null, false),
-    ("ru",    "Col",                      "К колоссянам",     "Кол.", null, false),
-    ("ru", "1Thess",             "К фессалоникийцам 1-е",    "1Фес.", null, false),
-    ("ru", "2Thess",             "К фессалоникийцам 2-е",    "2Фес.", null, false),
-    ("ru",   "1Tim",                     "К Тимофею 1-е",    "1Тим.", null, false),
-    ("ru",   "2Tim",                     "К Тимофею 2-е",    "2Тим.", null, false),
-    ("ru",    "Tit",                            "К Титу",     "Тит.", null, false),
-    ("ru",    "Phm",                        "К Филимону",     "Флм.", null, false),
-    ("ru",   "Hebr",                          "К евреям",     "Евр.", null, false),
-
-    ("ru", "ProvN",                 "Книги пророческие",        null, null,  null),
-
-    ("ru",  "Apoc",                       "Апокалипсис",     "Откр.", null, false);
+alter table `parallel` add primary key (`lang`, `src_book_id`, `src_chapter`, `src_verse`, `dst_book_id`, `dst_chapter`, `dst_verse`);
+create index `parallel_index` on `parallel` (`lang`, `src_book_id`, `src_chapter`, `src_verse`);
 
 # --- !Downs
 
-delete from `headers` where `lang` = "ru";
+drop table `parallel`;
+
+
+
